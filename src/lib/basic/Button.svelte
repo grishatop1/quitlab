85855dd1989d6eac5450b8f8147d0db1da08c00e
grisha <grigorije05@gmail.com>
2025-03-12 17:27:29 +0100

    initial

---
+++ src/lib/basic/Button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	let { children, ...props }: HTMLButtonAttributes = $props();
+</script>
+
+<button {...props}>
+	{@render children?.()}
+</button>
+
+<style>
+	button {
+		background-color: var(--primary);
+		border: 0;
+		border-radius: 6px;
+		color: white;
+		padding: 12px;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>QuitLab</title>
+</svelte:head>
+
+{@render children()}
