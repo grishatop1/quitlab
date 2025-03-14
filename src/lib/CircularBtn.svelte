10bff825ad24da018cc2194c7b5f1d2ba36b8be5
grisha <grigorije05@gmail.com>
2025-03-14 02:04:19 +0100

    improved plus btn

--- src/lib/CircularBtn.svelte
+++ src/lib/CircularBtn.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import Button from './basic/Button.svelte';
+	import Plus from './icons/Plus.svelte';
 	import type { HTMLButtonAttributes } from 'svelte/elements';
 
 	let { ...props }: HTMLButtonAttributes = $props();
 </script>
 
-<Button style="border-radius: 50%; width: 60px; height: 60px; font-size: 1.3em;" {...props}
-	>+</Button
+<Button
+	style="border-radius: 50%; width: 60px; height: 60px; display: flex; justify-content: center; align-items: center;"
+	{...props}
 >
+	<Plus />
+</Button>
