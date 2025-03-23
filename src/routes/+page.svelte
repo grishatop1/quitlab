f71922c8c55d4f1c560b6a05ab79c586f6619b41
grisha <grigorije05@gmail.com>
2025-03-23 16:34:53 +0100

    metadata for empty page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<h1>soon :)</h1>
+<svelte:head>
+	<title>QuitLab</title>
+	<meta property="og:title" content="QuitLab" />
+	<meta property="og:site_name" content="QuitLab" />
+	<meta property="og:description" content="Start making a change." />
+	<meta property="og:image" content="https://quitlab.org/ql.png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:url" content="https://quitlab.org" />
+	<meta property="og:type" content="website" />
+</svelte:head>
