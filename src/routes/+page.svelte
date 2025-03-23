77a1dba1cce871bde09bc1b198b63f9c58f68217
grisha <grigorije05@gmail.com>
2025-03-23 16:05:26 +0100

    metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,6 +39,17 @@
 	};
 </script>
 
+<svelte:head>
+	<meta property="og:title" content="QuitLab" />
+	<meta property="og:site_name" content="QuitLab" />
+	<meta property="og:description" content="Start making a change." />
+	<meta property="og:image" content="https://quitlab.org/ql.png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:url" content="https://quitlab.org" />
+	<meta property="og:type" content="website" />
+</svelte:head>
+
 {#if entries}
 	{#if entries.length > 0}
 		<div class="list">
