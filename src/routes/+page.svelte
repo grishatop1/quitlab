1076d8950f3e4b478fb60b8820970dbde13cb770
grisha <grigorije05@gmail.com>
2025-03-24 16:32:48 +0100

    update meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <svelte:head>
-	<title>QuitLab</title>
-	<meta property="og:title" content="QuitLab" />
-	<meta property="og:site_name" content="QuitLab" />
+	<title>QuitLab: Start making a change</title>
+	<meta property="og:title" content="QuitLab: Start making a change" />
+	<meta property="og:site_name" content="QuitLab: Start making a change" />
 	<meta
 		property="og:description"
-		content="Start making a change. AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+		content="QuitLab is a powerful yet simple tool designed to help you break free from bad habits and addictions. With a clean and intuitive interface, QuitLab makes tracking your progress easy, keeping you motivated along the way."
 	/>
 	<meta property="og:image" content="https://quitlab.org/ql.png" />
 	<meta property="og:url" content="https://quitlab.org" />
