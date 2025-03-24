7bea8d08c5ebc0f8e29486425021181ae05e698c
grisha <grigorije05@gmail.com>
2025-03-24 16:29:34 +0100

    remove w and h

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,11 @@
 	<title>QuitLab</title>
 	<meta property="og:title" content="QuitLab" />
 	<meta property="og:site_name" content="QuitLab" />
-	<meta property="og:description" content="Start making a change." />
+	<meta
+		property="og:description"
+		content="Start making a change. AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+	/>
 	<meta property="og:image" content="https://quitlab.org/ql.png" />
-	<meta property="og:image:width" content="1200" />
-	<meta property="og:image:height" content="630" />
 	<meta property="og:url" content="https://quitlab.org" />
 	<meta property="og:type" content="website" />
 </svelte:head>
